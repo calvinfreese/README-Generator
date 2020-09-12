dee92950d5496956143d77a28095176ea29344ac
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-09-12 14:53:02 -0500

    added conditionals for badge/license information

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,31 +1,60 @@
 // function to generate markdown for README
-const { makeBadge } = require('badge-maker');
 
+const badges = [
+  {
+    name: "Apache 2.0",
+    badge: "https://img.shields.io/badge/License-Apache%202.0-blue.svg)]",
+    url: "https://opensource.org/licenses/Apache-2.0"
+  },
+  {
+    name: "MIT",
+    badge: "https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT",
+    url: "https://opensource.org/licenses/MIT"
+  },
+  {
+    name: "ISC",
+    badge: "https://img.shields.io/badge/License-ISC-blue.svg",
+    url: "https://opensource.org/licenses/ISC"
+  },
+  {
+    name: "Unlicense",
+    badge: "https://img.shields.io/badge/license-Unlicense-blue.svg",
+    url: "http://unlicense.org/"
+  }
+]
 
 //takes data and inputs it into .md file
 const generateMarkdown = function(data) {
-  //format from badge maker
-  const format = {
-    label: 'License',
-    message: `${data.license}`,
-    color: 'green',
-  }
-
-  const badge = makeBadge(format);
-  const licensePath = `../utils/${data.license}.txt`;
+  
+ 
+ let badge = ''
+ let licenseURL = ''
+ if (data.license === "MIT") {
+   badge = badges[1].badge;
+   licenseURL = badges[1].url;
+ } else if (data.license === "Apache 2.0") {
+   badge = badges[0].badge;
+   licenseURL = badges[1].url;
+ } else if (data.license === "ISC") {
+   badge = badges[2].badge;
+   licenseURL = badges[2].url;
+ } else {
+   badge = badges[3].badge;
+   licenseURL = badges[3].url;
+ }
 
   
   return `
   # Title: ${data.title}
   ## Description 
   ${data.description}
-  ${badge}
+  ![License: ${data.license}](${badge})
   #### Table of Contents
   * [Description](#Description)
   * [Installation](#Installation)
   * [Usage](#Usage)
   * [License](#License)
-  * [Contributing](#Contributing)
+  * [Contributing](#Credits)
   * [Tests](#Tests)
   * [Questions](#Questions)
 
@@ -36,7 +65,8 @@ const generateMarkdown = function(data) {
   ${data.usage}
 
   ## License
-  * [${data.license}](${licensePath})
+  * [${data.license}](${licenseURL}) - click to view license information.
+
   
   ## Credits
   ${data.credit}
@@ -45,7 +75,9 @@ const generateMarkdown = function(data) {
   ${data.test}
 
   ## Questions
-  ${data.questions}
+  If there are any questions regarding this project, please reach me at:
+   * GitHub: [${data.github}](https://github.com/${data.github})
+   * Click to [Email Me](mailto:${data.email})
 `;
 }
 
